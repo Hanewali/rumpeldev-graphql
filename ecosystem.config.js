b608e218d8c780fe228b679bff64b01de5b80a70
Jakub Rumpel <jakub@rumpel.dev>
2022-05-01 10:53:01 +0200

    Added ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	apps : [
+		{
+				name: "rumpeldev",
+				script: "./server.js",
+				watch: true,
+				env: {
+						"DBUSER":"secret_DBUSER",
+						"DBPASSWORD": "secret_DBPASSWORD",
+						"DBURL": "secret_DBURL",
+						"DBPORT": secret_DBPORT,
+						"DBNAME": "secret_DBNAME"
+				}
+		}
+	]
+}
